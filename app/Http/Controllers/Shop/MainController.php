6dadd91c1f5c5353eb80b87ebc25ffca730f1d4d
Kolia-ANOH <colectionmac@gmail.com>
2024-09-15 13:12:23 +0200

    màj

--- app/Http/Controllers/Shop/MainController.php
+++ app/Http/Controllers/Shop/MainController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Shop;
 
 use App\Http\Controllers\Controller;
+use App\Models\Produit;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index() {
+
+        $produits = Produit::all();
+        dd($produits);
+
         return view("Shop.index");
     }
 }
